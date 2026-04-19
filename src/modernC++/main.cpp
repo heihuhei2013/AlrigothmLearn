168252cf69590dc6f3e6acdc9b8fe71455655629
betawu <betawu@foxmail.com>
2026-04-19 23:41:45 +0800

    modern c++

---
+++ src/modernC++/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by zuzu on 2026/4/19.
+//
+
+#include <iostream>
+#include <ostream>
+
+int main(int argc, char *argv[]) {
+    std::cout << "ModernC++:Hello World!" << std::endl;
+    return 0;
+}
